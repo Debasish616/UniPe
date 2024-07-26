238cee5a08ed4806c3936df7ff00ad76173cdc37
Debasish Behera <70677881+Debasish616@users.noreply.github.com>
2024-07-26 15:23:37 +0530

    Title

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app.routers import auth
 
-app = FastAPI()
+app = FastAPI(
+    title="UniPe",
+    description="This is the authentication server for UniPe. This will be used in all the UniPe services to authenticate the users.",
+    version="1.0.0"
+)
 
 app.include_router(auth.router)
 
